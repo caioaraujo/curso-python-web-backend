8d2a07bfea4f4640039a601f1615752752203cd4
caioaraujo <c.felipe.araujo@gmail.com>
2020-02-15 22:15:12 -0300

    modulo 3

---
+++ 3-Variaveis_e_manipulacao_de_strings/main.py
@@ -0,0 +1,9 @@
+produto = input("Digite o nome do produto: ")
+print("O produto possui", len(produto), "caracteres")
+
+quantidade = int(input("Digite a quantidade de itens: "))
+
+valor = float(input("Digite o valor total: "))
+
+saida = f"Produto: {produto}, Qtde.: {quantidade}, Valor: {valor}"
+print(saida)
