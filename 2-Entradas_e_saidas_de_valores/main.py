b02c2afaaa056e69e9e135b01143f6cddd9775aa
caioaraujo <c.felipe.araujo@gmail.com>
2020-02-15 22:05:43 -0300

    Modulo 2

---
+++ 2-Entradas_e_saidas_de_valores/main.py
@@ -0,0 +1,7 @@
+produto = input("Digite o nome do produto: ")
+
+quantidade = int(input("Digite a quantidade de itens: "))
+
+valor = float(input("Digite o valor total: "))
+
+print("Produto:", produto, "Qtde.:", quantidade, "Valor:", valor)
